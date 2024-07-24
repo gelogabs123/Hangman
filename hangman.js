bf9050f1ed653295f1cd317596ee90060d4013bb
gelogabs123 <gelogabs123@gmail.com>
2024-07-25 00:08:26 +0100

    seperate letters and started on keyboard
    Words are now in jagged array and keyboard is set up roughly with disabling working when clicked.

--- hangman.js
+++ hangman.js
@@ -1,12 +1,21 @@
-var words = ['Horse',
-    'Pig',
-    'Dog',
-    'Cat',
-    'Parrot',
-    'Iguana'
+var words = [
+        ['h','o','r','s','e'],
+        ['p','i','g'],
+        ['d','o','g'],
+        ['c','a','t'],
+        ['p','a','r','r','o','t'],
+        ['i','g','u','a','n','a']
     ];
 
 function generateWord() {
     let randWord = words[Math.floor(Math.random() * words.length)];
     document.getElementById('word-display').innerHTML = randWord;
+}
+
+function checkLetter(letterbtn) {
+
+}
+
+function clearLetter(letterbtn) {
+    letterbtn.disabled = true;
 }
